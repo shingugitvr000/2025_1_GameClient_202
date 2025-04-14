9fcc4c45ac46179688e2ab8e53fbfb07cb251edc
Shingugitvr000 <Shingugitvr000@gmail.com>
2025-04-14 11:07:27 +0900

    [Feat] 카드게임 카드 설정 , 카드 드레그 앤 드롭, 카드 효과 발동

---
+++ Assets/Scripts/CardGame/CaracterStats.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class CaracterStats : MonoBehaviour
+{
+    public string characterName;
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    //UI ¿ä¼Ò
+    public Slider healthBar;
+    public TextMeshProUGUI healthText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth += amount;
+    }
+}
