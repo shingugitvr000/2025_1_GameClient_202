a942420bce9d22cdcca1dd05d8292d0768b7f5c3
Shingugitvr000 <Shingugitvr000@gmail.com>
2025-05-12 11:03:14 +0900

    [Feat] 카드 추가 효과 (드로우, 디스카드, 마나 획독)

--- Assets/Scripts/CardGame/CardData.cs
+++ Assets/Scripts/CardGame/CardData.cs
@@ -14,6 +14,19 @@ public class CardData : ScriptableObject
         Utility                 //유틸리티 카드 
     }
 
+    public enum AdditionalEffectType                    //추가 효과 타입 열거형 추가 
+    {
+        None,                           //추가 효과 없음
+        DrawCard,                       //카드 드로우
+        DiscardCard,                    //카드 버리기
+        GainMana,                       //마나 획득
+        ReduceEnemyMana,                //적 마나 감소
+        ReduceCardCost                  //다음 카드 비용 감소 
+    }
+
+    //추가 효과 리스트 
+    public List<AdditionalEffect> additionalEffects = new List<AdditionalEffect>();
+
     public string cardName;             //카드 이름
     public string description;          //카드 설명
     public Sprite artwork;              //카드 이미지
@@ -37,5 +50,21 @@ public class CardData : ScriptableObject
                 return Color.white;
 
         }
+    }
+
+    //추가 효과 정보를 문자열로 변환
+    public string GetAdditionalEffectsDescription()
+    {
+        if (additionalEffects.Count == 0)
+            return "";
+
+        string result = "\n";
+
+        foreach(var effect in additionalEffects)
+        {
+            result += effect.GetDescription() + "\n";
+        }
+
+        return result;
     }
 }
